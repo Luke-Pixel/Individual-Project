5d13202b06886e4f4789fca0b68eb477568dfe6c
Luke <Luke1014@live.co.uk>
2019-12-28 12:29:22 +0000

    28/12/19

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="home.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+  </head>
+<body>
+
+    
+            
+        <aside>
+            <figure>
+                <div id="avatar"></div>
+                <figcaption>Johnny Doe</figcaption>
+            </figure>
+            <img src="images/menu.svg" class = "Menu_Bar">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About me</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <h1 class = "header1">Welcome Back</h1>
+        <form class = "Home_1">
+        <main>
