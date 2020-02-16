23313804a717d5c798ca8c6f6c45f29430b75d1c
Luke <Luke1014@live.co.uk>
2020-02-16 20:39:28 +0000

    16/02/2020

---
+++ includes/addScrrening.php
@@ -0,0 +1,70 @@
+<?php
+if (isset($_POST['submit_screening'])){
+    //require 'dbcon.php';
+
+    $chl;
+    $gon;
+    $syph;
+    $hiv;
+    $tstDate;
+    
+    //check selection for chlym
+    if(isset($_POST['Chlamydia']) == 'chlamydia_na'){
+        $chl  = 'Not Applicable';
+    }else if(isset($_POST['Chlamydia']) == 'chlamydia_yes'){
+        $chl = 'Positive';
+    }else if(isset($_POST['Chlamydia']) == 'chlamydia_no'){
+        $chl = 'Negative';
+    }else{
+        //send back to page with uncheck fields 
+        header("Location: ../newscreening.html?error=nochlselect");
+        exit();
+    }
+
+    //check selection for gon
+    if(isset($_POST['Gonorrhea']) == 'gonorrhea_na'){
+        $gon = 'Not Applicable';
+    }else if(isset($_POST['Gonorrhea']) == 'gonorrhea_yes'){
+        $gon = 'Positive';
+    }else if(isset($_POST['Gonorrhea']) == 'gonorrhea_no'){
+        $gon = 'Negative';
+    }else{
+        //send back to page with uncheck fields 
+        header("Location: ../newscreening.html?error=nogonselect");
+        exit();
+    }
+
+    //check selection for gon
+    if(isset($_POST['Syphilis']) == 'syphilis_na'){
+        $syph = 'Not Applicable';
+    }else if(isset($_POST['Syphilis']) == 'syphilis_yes'){
+        $syph = 'Positive';
+    }else if(isset($_POST['Syphilis']) == 'syphilis_na'){
+        $syph = 'Negative';
+    }else{
+        //send back to page with uncheck fields 
+        header("Location: ../newscreening.html?error=nosyphselect");
+        exit();
+    }
+
+    //check selection for gon
+    if(isset($_POST['HIV']) == 'hiv_na'){
+        $hiv = 'Not Applicable';
+    }else if(isset($_POST['HIV']) == 'hiv_yes'){
+        $hiv = 'Positive';
+    }else if(isset($_POST['hiv_no']) == 'hiv_no'){
+        $hiv= 'Negative';
+    }else{
+        //send back to page with uncheck fields 
+        header("Location: ../newscreening.html?error=nohivselect");
+        exit();
+    }
+
+    if($_POST['date'] == ''){
+        header("Location: ../newscreening.html?error=nodate");
+        exit();
+    }
+
+
+    
+}
--- includes/interview.php
+++ includes/interview.php
@@ -14,7 +14,9 @@ if (isset($_POST['sighnup-submit'])){
     $smoke = isset($_POST['smoke_yes']);
     $currentDate = date ('Y/m/d');
 
-    $id = echo $_SESSION['userid'];
+    $trubl = 1
+
+    $id = $_SESSION['userid'];
     $sql = "INSERT INTO Interview (interview_id, date_completed, latest) VALUES (1,?,?)";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -22,7 +24,7 @@ if (isset($_POST['sighnup-submit'])){
         exit();
     }else{
         
-        mysqli_stmt_bind_param($stmt,"si",$currentDate, True);
+        mysqli_stmt_bind_param($stmt,"si",$currentDate, $trubl);
         mysqli_stmt_execute($stmt);    
 
         $sql = "INSERT INTO Fitness VALUES (1,?,?)";
@@ -40,7 +42,7 @@ if (isset($_POST['sighnup-submit'])){
                 header("Location: ../sighnup.php?error=connectionerrorinsert&email=" .$email);
             exit();
             }else{
-                mysqli_stmt_bind_param($stmt,"ii",$excercise, $smoke);
+                mysqli_stmt_bind_param($stmt,"iiiii",$cutdrink, $annoying, $feltbad, $drinkmorn, $clubdrug);
                 mysqli_stmt_execute($stmt);
 
                 $sql = "INSERT INTO Happy VALUES (1,?)";
@@ -49,11 +51,20 @@ if (isset($_POST['sighnup-submit'])){
                     header("Location: ../sighnup.php?error=connectionerrorinsert&email=" .$email);
                 exit();
                 }else{
-                    mysqli_stmt_bind_param($stmt,"ii",$excercise, $smoke);
+                    mysqli_stmt_bind_param($stmt,"i",$mental);
                     mysqli_stmt_execute($stmt);
 
-                    $sql = "INSERT INTO Happy VALUES (1,?)";
+                    $sql = "INSERT INTO Out VALUES (1,?)";
                     $stmt = mysqli_stmt_init($conn);
+                    if(!mysqli_stmt_prepare($stmt, $sql)){
+                        header("Location: ../sighnup.php?error=connectionerrorinsert&email=" .$email);
+                    exit();
+                    }else{
+                        mysqli_stmt_bind_param($stmt,"ii",$out);
+                        mysqli_stmt_execute($stmt);
+
+
+                    }
                 }
             }
         }
